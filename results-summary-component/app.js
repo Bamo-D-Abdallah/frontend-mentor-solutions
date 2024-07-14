afd3e8c15892bbf8744f72650f0023c1076d8ff1
Bamo Dara <bamo.dara.abdallah@gmail.com>
2024-07-14 03:40:25 +0300

    result-summary

--- results-summary-component/app.js
+++ results-summary-component/app.js
@@ -4,13 +4,16 @@ const categoryContainer = document.querySelector('.result-card__category-contain
 (async function() {
     const jsonFile = await fetch("./data.json");
     const categories = await jsonFile.json();
-    const colors = ["read", "yellow", "green", "blue"];
+    const colors = ["red", "yellow", "green", "blue"];
 
     for(let i = 0; i < categories.length; i++) {
         const categoryElement = document.createElement("div");
         categoryElement.classList.add("result-card__category");
-        categoryElement.classList.add("result-card__category--" + colors[i]);
+        categoryElement.classList.add(colors[i] + "-background");
 
+
+        const categoryContentContainer = document.createElement("div");
+        categoryContentContainer.classList.add("result-card__category-content");
 
         const categoryIconElement = document.createElement("img");
         categoryIconElement.src = categories[i].icon;
@@ -19,16 +22,25 @@ const categoryContainer = document.querySelector('.result-card__category-contain
         const categoryTextElement = document.createElement("p");
         categoryTextElement.textContent = categories[i].category;
         categoryTextElement.classList.add("result-card__category-text");
+        categoryTextElement.classList.add(colors[i] + "-color");
 
+
+
+        categoryContentContainer.append(categoryIconElement);
+        categoryContentContainer.append(categoryTextElement);
 
 
         const categoryScoreElement = document.createElement("p");
+
+        const categoryScoreMax = document.createElement("span");
+        categoryScoreMax.innerText = " / 100";
+
         categoryScoreElement.innerText = categories[i].score + "";
         categoryScoreElement.classList.add("result-card__category-score");
 
+        categoryScoreElement.append(categoryScoreMax);
 
-        categoryElement.append(categoryIconElement);
-        categoryElement.append(categoryTextElement);
+        categoryElement.append(categoryContentContainer);
         categoryElement.append(categoryScoreElement);
 
         categoryContainer.append(categoryElement);
